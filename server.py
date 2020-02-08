31a2c9a170e9d7f65f5ac65251ca7fb525f336b4
Mirza Baig <baig1995@hotmail.com>
2020-02-07 16:59:07 -0800

    Dojo fruit shop updated

--- server.py
+++ server.py
@@ -8,7 +8,17 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    strawberry = request.form['strawberry']
+    raspberry = request.form['raspberry']
+    apple = request.form['apple']
+    name = request.form['first_name']
+    last_name = request.form['last_name']
+    studentId = request.form['student_id']
+
+    total = int(strawberry)+int(raspberry)+int(apple)
+    print("Charging " + name + " " + last_name + " for " + str(total) + " fruits")
+
+    return render_template("checkout.html",strawberry=strawberry,raspberry=raspberry,apple=apple,first_name=name,last_name=last_name,student_id=studentId, total=total)
 
 @app.route('/fruits')         
 def fruits():
